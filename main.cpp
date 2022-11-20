ff7f920f40d3256829a1eab03e06efcf14ca5b56
goldcirrus <53105370+goldcirrus@users.noreply.github.com>
2022-11-19 21:36:41 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int x=9;
+    int &y=x;
+
+    float  z = 2.3;
+    if(0.01)
+      printf("y is %d \n", y);
+
+    cout << "INT max is "<<INT_MAX<<endl;
+
+    return 0;
+}
